66b6ee5dedc43f2b2c3e677ea60575af3242a1d5
Patrik Morell <patrik.morell@knowit.se>
2024-02-28 07:36:02 +0100

    "Added routing"

--- Frontend/App.tsx
+++ Frontend/App.tsx
@@ -1,46 +1,19 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet, Button, ScrollView } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createMaterialBottomTabNavigator } from "react-native-paper/react-navigation";
+import HomeScreen from "./screens/HomeScreen";
+import SettingsScreen from "./screens/SettingsScreen";
 
+const Tab = createMaterialBottomTabNavigator();
 const App = () => {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  const fetchWeatherData = () => {
-    setLoading(true);
-    setError(null);
-
-    fetch("http://localhost:5213/WeatherForecast")
-      .then((response) => response.json())
-      .then((data) => {
-        setData(data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error("There was an error!", error);
-        setError(error);
-        setLoading(false);
-      });
-  };
-
   return (
-    <View style={styles.container}>
-      <Button title="Load Weather Data" onPress={fetchWeatherData} />
-
-      {loading && <Text>Loading...</Text>}
-      {error && <Text>Error fetching data.... {JSON.stringify(error)}</Text>}
-      {data && <Text>{JSON.stringify(data, null, 2)}</Text>}
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 20,
-  },
-});
 
 export default App;
--- Frontend/screens/HomeScreen.tsx
+++ Frontend/screens/HomeScreen.tsx
@@ -1,5 +1,44 @@
-import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+import React, { useState } from "react";
 
 export default function HomeScreen() {
-  return <div>HomeScreen</div>;
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchWeatherData = () => {
+    setLoading(true);
+    setError(null);
+
+    fetch("http://localhost:5213/WeatherForecast")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("There was an error!", error);
+        setError(error);
+        setLoading(false);
+      });
+  };
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+      <Button title="Load Weather Data" onPress={fetchWeatherData} />
+
+      {loading && <Text>Loading...</Text>}
+      {error && <Text>Error fetching data.... {JSON.stringify(error)}</Text>}
+      {data && <Text>{JSON.stringify(data, null, 2)}</Text>}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+});
